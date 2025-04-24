c589af8f536c4a33962f75b30ccaef3bb0a1b380
Diego <didev4207@gmail.com>
2025-04-24 16:53:57 -0400

    Terminar Part.1

--- Sources/main.js
+++ Sources/main.js
@@ -1,1 +1,19 @@
-console.log("hola");
+const images = [
+  "https://www.puntacanatravelblog.com/wp-content/uploads/dominican_republic_095_santo_domingo_zona_colonial_alcazar_de_colon-1024x768.jpg",
+  "https://mostinterestingdestinations.com/wp-content/uploads/2022/11/Zona-Colonial.jpg",
+  "https://vagabuzz.com/img/blog/plaza-epana-santo-domingo.jpg",
+];
+const imageElement = document.getElementById("img_slider");
+let savedIndx = 0;
+
+const changeImage = () => {
+  if (savedIndx >= images.length) {
+    savedIndx = 0;
+    return;
+  }
+  imageElement.src = images[savedIndx];
+  savedIndx += 1;
+  console.log("Image Changed");
+};
+
+setInterval(changeImage, 5000);
